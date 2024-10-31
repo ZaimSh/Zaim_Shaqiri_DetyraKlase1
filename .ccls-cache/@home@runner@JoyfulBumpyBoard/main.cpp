54f15cca4c990f944d160132dad23ff6fe8ca9cc
ZaimSh <shaqiriximi@gmail.com>
2024-10-31 15:06:19 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@JoyfulBumpyBoard/main.cpp
@@ -0,0 +1,21 @@
+// Zaim Shaqiri
+// 31/10/2024
+// Programi me demonstrim te hyrjes dhe daljes.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+  int x;
+
+  cout<<"Vlera e x eshte: \n";
+  cin>>x;
+
+  x>0 ? cout<<"\nNumri " <<x <<" eshte pozitiv\n" : cout<<"\nNumri " <<x <<" eshte negativ\n";
+  
+  
+}
